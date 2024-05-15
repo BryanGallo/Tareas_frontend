3cdcc02eedb5c376d7f261093cd0a6b079af7a8a
Bryan Gallo <bryangallo95@hotmail.com>
2024-05-14 22:53:52 -0500

    Inicio componente Forgot y ruta en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./views/Login";
-
+import Login from "./views/Auth/Login";
+import ForgotPassword from "./views/Auth/ForgotPassword"
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/">
                     <Route index element={<Login />} />
+                    <Route
+                        path="auth/forgot-password"
+                        element={<ForgotPassword />}
+                    />
                 </Route>
             </Routes>
         </BrowserRouter>
---
+++ src/views/Auth/ForgotPassword.jsx
@@ -0,0 +1,7 @@
+export default function ForgotPassword() {
+    return (
+        <div>
+            <h1>Olvidaste Contraseña</h1>
+        </div>
+    );
+}
